724017617f32b4a21d17efe25a0f8f68688452a5
Innocent ThankGod <thankgodinnocent342@gmail.com>
2025-01-24 19:28:01 +0100

    initial  commit

---
+++ ECommerceAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ECommerceAPI.Model;
+using Microsoft.EntityFrameworkCore;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<AppCustomer> AppCustomers { get; set; }
+
+}
+
---
+++ ECommerceAPI/Model/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerceAPI.Model
+{
+    public class Order
+    {
+
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public DateTime OrderDate { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalAmount { get; set; }
+        public string OrderStatus { get; set; }
+    }
+}
